626ede0505d97e38da96be9bf39d678db2dffd92
James Tombleson <luther38@gmail.com>
2019-03-19 06:29:56 -0700

    Adjustment for Pester

--- Run-UnitTests.ps1
+++ Run-UnitTests.ps1
@@ -9,6 +9,4 @@ Import-Module "$PSSCRIPTROOT\Chainsaw\Chainsaw.psm1" -Force
 
 # Import Pester to run our tests
 Import-Module Pester
-Invoke-Pester -Script "$($PSSCRIPTROOT)\Tests\TargetConsole.Test.ps1"
-Invoke-Pester -Script "$($PSSCRIPTROOT)\Tests\TargetCSV.Test.ps1"
-Invoke-Pester -Script "$($PSSCRIPTROOT)\Tests\TargetTeams.Test.ps1"
+Invoke-Pester -Script "$($PSSCRIPTROOT)\Tests\"
